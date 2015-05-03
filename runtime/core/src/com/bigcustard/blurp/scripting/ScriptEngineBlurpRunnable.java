b08444b9fd4701145634945f9f54402e9b2df933
phil-anderson <climberphil@gmail.com>
2015-05-03 11:36:46 +0100

    SpenceSession

--- runtime/core/src/com/bigcustard/blurp/scripting/ScriptEngineBlurpRunnable.java
+++ runtime/core/src/com/bigcustard/blurp/scripting/ScriptEngineBlurpRunnable.java
@@ -25,7 +25,7 @@ public class ScriptEngineBlurpRunnable implements BlurpRunnable {
     }
 
     @Override
-    public void run(Blurp blurp, Screen screen, Keyboard keyboard, Utils utils, Effects effects) {
+    public void run(Blurp blurp, Screen screen, Keyboard keyboard, Utils utils) {
 
         Bindings bindings = scriptEngine.createBindings();
         bindings.put("blurp", blurp);
